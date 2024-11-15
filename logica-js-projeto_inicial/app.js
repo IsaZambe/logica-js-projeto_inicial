faea562b15389b018801eb65b19d9b65bf772fb7
Isa Zambe <isabela@zambe.com.br>
2024-11-15 10:52:10 -0300

    Inclusão de 'else'

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,7 +1,14 @@
 alert('Boas vindas ao jogo do número secreto!'); //entre aspas é um texto
 let numeroSecreto = 29;
+console.log(numeroSecreto);
 let chute = prompt('Escolha um número entre 1 e 30');
 
+// se o chute for igual ao numero secreto, ai executa uma mensagem
 if (numeroSecreto == chute) {
-    console.log('Parabéns! Você descobriu o número secreto (29)');
+    alert('Parabéns! Você descobriu o número secreto (29)');
+}
+
+//caso o chute não seja igual ao numero secreto, outra coisa será executado.
+else {
+    alert('ERRRRRRRROOOOOU :)');
 }
