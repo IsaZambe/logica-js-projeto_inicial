613f9fbcac80f7a669214fa349a7f06e9ae13418
Isa Zambe <isabela@zambe.com.br>
2024-11-15 15:55:25 -0300

    Adionado número de tentativas

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,19 +1,28 @@
 alert('Boas vindas ao jogo do número secreto!'); //entre aspas é um texto
 let numeroSecreto = 29;
 console.log(numeroSecreto);
-let chute = prompt('Escolha um número entre 1 e 30');
-console.log('Valor do chute:', chute); //aparece o valor do chute do jogador
+let chute;
+let tentativas = 1;
 
-// console.log('Resultado da comparação:', chute == numeroSecreto); < mostra se o chute foi verdadeiro ou falso
+// enquanto chute não for igual ao numero secreto
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 100');
 
-// se o chute for igual ao numero secreto, ai executa uma mensagem
-if (numeroSecreto == chute) {
-    alert(`Parabéns! Você descobriu o número secreto ${numeroSecreto}`);
-}
+    // se o chute for igual ao numero secreto, ai executa uma mensagem
+    if (chute == numeroSecreto) {
+        alert(`Parabéns! Você descobriu o número secreto (${numeroSecreto}) com ${tentativas} tentativa(s).`);
+        }
 
-//caso o chute não seja igual ao numero secreto, outra coisa será executado.
-else {
-    // console.log('Valor do número secreto:', numeroSecreto); > mostra no console o valor do numero secreto novamente, deve ser escrito antes do alerta.
-    alert('ERRRRRRRROOOOOU :)');
-    
+    //caso o chute não seja igual ao numero secreto, outra coisa será executado.
+    else {
+        if (numeroSecreto > chute) {
+            alert(`O número secreto é maior que ${chute}`);
+        } 
+        else {
+            alert(`O número secreto é menor que ${chute}`);
+        }
+        //tentativas = tentativas + 1;
+        tentativas++;
+                  
+    }   
 }
