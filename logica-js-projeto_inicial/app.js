76a6357250fce39fdded2061f2609d6a45f9c965
Isa Zambe <isabela@zambe.com.br>
2024-11-15 10:25:11 -0300

    Iniciando aula de lógica de programação com JavaScript

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,7 @@
+alert('Boas vindas ao jogo do número secreto!'); //entre aspas é um texto
+let numeroSecreto = 29;
+let chute = prompt('Escolha um número entre 1 e 30');
+
+if (numeroSecreto == chute) {
+    console.log('Parabéns! Você descobriu o número secreto (29)');
+}
